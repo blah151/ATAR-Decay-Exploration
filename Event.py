f278773c0c87d7a702b5712deb343b985ca29558
Cole Welch <colewelch151@gmail.com>
2021-08-03 18:38:48 -0700

    Plotted edep per plane and added Event class; this implementation is still incomplete.

---
+++ Event.py
@@ -0,0 +1,19 @@
+class Event():
+
+    # def __init__(self):
+    #     self.c_data = []
+    #     self.t_data = []
+    #     self.x_data = []
+    #     self.y_data = []
+    #     self.z_data = []
+    #     self.E_data = []
+    #     self.E_per_plane = []
+    
+    def __init__(self, c_data, t_data, x_data, y_data, z_data, E_data, E_per_plane):
+        self.c_data = c_data
+        self.t_data = t_data
+        self.x_data = x_data
+        self.y_data = y_data
+        self.z_data = z_data
+        self.E_data = E_data
+        self.E_per_plane = E_per_plane
