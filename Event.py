0fa85a190b48392b6ef4d00c4efb2cd45b2af2e4
Cole Welch <colewelch151@gmail.com>
2021-08-23 09:28:02 -0700

    Fixed unidentified particles being plotted as incorrect colors and now display their pdgs.

--- Event.py
+++ Event.py
@@ -1,11 +1,11 @@
 '''Class stores the following data for ease of use:
-c - list of colors to distinguish different decay products.
 t - time data.
 x - x-coordinate data.
 y - y-coordinate data.
 z - z-coordinate data; i.e., the number of planes deep into the ATAR
 E - energy deposited at the given read time
 E_per_plane - energy deposited per plane. Should be of length 50 since there are 50 planes.
+pixel_pdgs - particle IDs (as ints) to distinguish different decay products.
 max_E - maximum energy deposited over all planes.
 gap_times - any large time gaps in the data, which signal a decay at rest.
 '''
@@ -15,12 +15,12 @@ import numpy as np
 class Event():
 
     def __init__(self):
-        self.c_data = []
         self.t_data = []
         self.x_data = []
         self.y_data = []
         self.z_data = []
         self.E_data = []
         self.E_per_plane = np.zeros(50)
+        self.pixel_pdgs = []
         self.max_E = []
         self.gap_times = []
