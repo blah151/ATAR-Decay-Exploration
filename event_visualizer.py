937a5695ad09f2c7327a0d41048c2b48c3e20043
Cole Welch <colewelch151@gmail.com>
2022-03-26 12:54:53 -0700

    Cleaned up the textual data shown, fixed x,y,z data not showing up on plots, and restricted x-axis ticks to 2 decimal places on z vs. t plots to reduce clutter.

--- event_visualizer.py
+++ event_visualizer.py
@@ -6,6 +6,7 @@ from logging.config import IDENTIFIER
 import ROOT as r
 import numpy as np
 from matplotlib import pyplot as plt
+from matplotlib.ticker import StrMethodFormatter
 from Event import Event
 import calo_analysis
 
@@ -13,11 +14,11 @@ import calo_analysis
 class Event_Visualizer:
     #TODO: Deal with is_event_DAR along with select_events()
     '''
-    Combines the functions we created above to give a visualization of events with the specified condition(s).
+    The primary method that this class is used for. Uses helper functions below to give a visualization of events satisfying the specified condition(s).
     is_event_DAR: Value of 0 = decays in flight, 1 = decays at rest, 2 = all data used.
     display_text_output = True / False controls whether we should have text info / not have text info displayed.
     display_plots = True / False controls whether event data is plotted or not.
-    num_events allows us to plot multiple events with the specified conditions from the tree.\
+    num_events allows us to plot multiple events with the specified conditions from the tree.
     gap_times = True / False means we should show / not show gap times between decays if any are present.
     '''
     def visualize_event(self, tree_atar, tree_calo, is_event_DAR, display_text_output, event_index):
@@ -82,6 +83,9 @@ class Event_Visualizer:
         
         #Extract x vs. t, y vs. t, and z vs. t data. Also add indexed color coding to represent different particles.
         for i in range(pixel_hits.size()):
+            #We can get the plane number using some modular arithmetic on the pixel_hits values. The convention is that these numbers start at 100,000,
+            #so we must subtract 100,000. We also have to subtract 1 to deal with 100 wrapping around to 0 when it shouldn't (i.e., an "off by one"
+            #error)
             plane = int(np.floor((pixel_hits[i] - 1 - 100_000) / npixels_per_plane))
 
             cur_val = (pixel_hits[i] - 1) % npixels_per_plane
@@ -90,6 +94,9 @@ class Event_Visualizer:
 
             event.t_data.append(cur_time)
 
+            #Even planes give us x-values while odd-numbered planes give us y-values. We can use some simple modular arithmetic to distinguish between
+            #the two. We must also add NaNs to fill in the gaps in x_data and y_data to make sure the indices of actual data points correspond to the
+            #timestamps recorded in t_data.
             if(plane % 2 == 0):
                 event.x_data.append(cur_val)
                 event.y_data.append(np.nan)
@@ -131,70 +138,22 @@ class Event_Visualizer:
         return event
 
 
-    #Show some useful parameters describing our event.
+    #Show some useful data describing our event in textual form.
     def display_event(self, event):
-        print("Length of pixel_pdgs: " + str(len(event.pixel_pdgs)))
-        print("Length of t_data: " + str(len(event.t_data)))
-        print("Length of x_data: " + str(len(event.x_data)))
-        print("Length of y_data: " + str(len(event.y_data)))
-        print("Length of z_data: " + str(len(event.z_data)))
-        print("Length of E_data: " + str(len(event.E_data)))
-        print("Length of E_per_plane: " + str(len(event.E_per_plane)))
-        print("pixel_pdgs: " + str(event.pixel_pdgs))
-        print("x_data: " + str(event.x_data))
-        print("t_data: " + str(event.t_data))
+        print("Length of pixel_pdgs: ", len(event.pixel_pdgs))
+        print("Length of x_data: ", len(event.x_data))
+        print("Length of y_data: ", len(event.y_data))
+        print("Length of z_data: ", len(event.z_data))
+        print("Length of t_data: ", len(event.t_data))
+        print("Length of E_data: ", len(event.E_data))
+        print("Length of E_per_plane: ", len(event.E_per_plane), "\n\n")
 
-
-    #For each color in a list of color labels for different particles, plot the corresponding data.
-    def plot_with_color_legend(self, x_coords, y_coords, pixel_pdgs):
-        #Store colors and corresponding particle type labels in one place for ease of editing.
-        colors = ["r", "b", "g", "y", "m"]
-        labels = ["Pion", "Positron", "Electron", "Antimuon", "Muon"]
-        particle_IDs = [211, -11, 11, -13, 13]
-        
-        #Store list of tuples of our data to be processed below.
-        coords = zip(x_coords, y_coords, pixel_pdgs)
-
-        #Keep track of known particles and other particles to plot.
-        cur_data_to_plot = []
-        other_to_plot = []
-        other_IDs = []
-        other_colors = ["k", "gray", "cyan", "indigo", "teal", "lime"]
-
-        #For each color, extract all pdgs of that color and display them on a scatter plot.
-        for i in range(0, len(particle_IDs)):
-            for coord in coords:
-                if coord[2] == particle_IDs[i]:                
-                    cur_data_to_plot.append(coord)
-                elif coord[2] not in particle_IDs:  # Keep track of number of unidentified particles so we can plot them separately after exiting this loop.
-                    other_to_plot.append(coord)
-
-                    #Keep list of other IDs so we know how many labels are needed.
-                    if coord[2] not in other_IDs:
-                        other_IDs.append(coord[2])
-                    #str(pixel_pdg[i])
-            
-            #Plot data for corresponding color each loop, but only if the data for the corresponding color is not empty.
-            if len(cur_data_to_plot) != 0:
-                plt.scatter([coord[0] for coord in cur_data_to_plot], [coord[1] for coord in cur_data_to_plot], 10, colors[i], label = labels[i])
-
-            #Reset data to plot every loop.
-            cur_data_to_plot = []
-
-        #Plot the unidentified particles in distinct colors with their pdgs for labels.
-        for i in range(0, len(other_IDs)):
-            ID = other_IDs[i]
-            
-            #Extract x and y coordinates to plot only if they are all of the particle ID that we are currently plotting.
-            # [coord[0] for coord in other_to_plot if coord[2] == ID]
-            x = []
-            y = []
-            for coord in other_to_plot:
-                if coord[2] == ID:
-                    x.append(coord[0])
-                    y.append(coord[1])
-
-            plt.scatter(x, y, 10, other_colors[i], label = str(ID))
+        print("pixel_pdgs: ", str(event.pixel_pdgs), "\n")
+        print("x_data:", event.x_data, "\n")
+        print("y_data:", event.y_data, "\n")
+        print("z_data:", event.z_data, "\n")
+        print("t_data:", event.t_data, "\n")
+        print("E_data:", event.E_data)
 
 
     #Plot the following data from our event: x vs. t, y vs. t, z vs. t, E vs. z, and energy deposited in calorimeter by (theta, phi). The graphs 
@@ -204,17 +163,11 @@ class Event_Visualizer:
         fig = plt.figure(figsize = (15, 10))
 
         plt.subplot(2,4,1)
-
-        print("x_data:", event.x_data)
-        print("y_data:", event.y_data)
-        print("z_data:", event.z_data)
-        print("t_data:", event.t_data)
-        print("E_data:", event.E_data)
-
         self.plot_with_color_legend(event.z_data, event.x_data, event.pixel_pdgs)
+        #plt.scatter([0, 5, 10, 15, 20, 25, 30], [0, np.nan, 10, np.nan, 20, np.nan, 30], 10, "r", label = "Pion")
         plt.title("x vs. z")
         plt.xlabel("z (plane number)")
-        plt.ylabel("x (pix)")
+        plt.ylabel("x (pixels)")
         plt.legend()
         plt.xlim(0, num_planes)
         plt.ylim(0, 100)
@@ -223,7 +176,7 @@ class Event_Visualizer:
         self.plot_with_color_legend(event.z_data, event.y_data, event.pixel_pdgs)
         plt.title("y vs. z")
         plt.xlabel("z (plane number)")
-        plt.ylabel("y (pix)")
+        plt.ylabel("y (pixels)")
         plt.xlim(0, num_planes)
         plt.ylim(0, 100)
 
@@ -232,11 +185,11 @@ class Event_Visualizer:
         plt.title("z vs. t")
         plt.xlabel("t (ns)")
         plt.ylabel("z (plane number)")
+        plt.gca().xaxis.set_major_formatter(StrMethodFormatter('{x:,.2f}'))     #Ensures only 2 decimal places are shown on x-axis, decreasing clutter.
         # plt.xlim(0, 60)
         plt.ylim(0, num_planes)
 
         plt.subplot(2,4,4)
-
         plt.scatter(event.z_data, event.E_data, 10, label = "e_dep")
         plt.scatter(range(50), event.E_per_plane, 10, "black", label = "e_dep per plane")
         plt.title("ATAR Energy Deposition Per Plane vs. z")
@@ -266,14 +219,64 @@ class Event_Visualizer:
             cbar.set_label('Amount of Energy Deposited')
 
         # plt.subplots_adjust(left = 0.1,
-        #                     bottom = 0.1, 
-        #                     right = 0.9, 
-        #                     top = 0.9, 
-        #                     wspace = 0.5, 
+        #                     bottom = 0.1,
+        #                     right = 0.9,
+        #                     top = 0.9,
+        #                     wspace = 0.5,
         #                     hspace = 0.4)
         
         plt.tight_layout()
         plt.show()
+
+
+    #For each color in a list of color labels for different particles, plot the corresponding data.
+    def plot_with_color_legend(self, x_coords, y_coords, pixel_pdgs):
+        #Store colors and corresponding particle type labels in one place for ease of editing.
+        colors = ["r", "b", "g", "y", "m"]
+        labels = ["Pion", "Positron", "Electron", "Antimuon", "Muon"]
+        particle_IDs = [211, -11, 11, -13, 13]
+        
+        #Store list of tuples of our data to be processed below.
+        coords = list(zip(x_coords, y_coords, pixel_pdgs))
+
+        #Keep track of known particles and other particles to plot.
+        cur_data_to_plot = []
+        other_to_plot = []
+        other_IDs = []
+        other_colors = ["k", "gray", "cyan", "indigo", "teal", "lime"]
+
+        #For each color, extract all pdgs of that color and display them on a scatter plot.
+        for i in range(0, len(particle_IDs)):
+            for coord in coords:
+                if coord[2] == particle_IDs[i]:
+                    cur_data_to_plot.append(coord)
+                elif coord[2] not in particle_IDs:
+                    #Keep track of unidentified particles and their IDs so we can plot them separately after exiting this loop.
+                    other_to_plot.append(coord)
+                    if coord[2] not in other_IDs:
+                        other_IDs.append(coord[2])
+            
+            #Plot data for corresponding color each loop, but only if the data for the corresponding color is not empty.
+            if len(cur_data_to_plot) != 0:
+                plt.scatter([coord[0] for coord in cur_data_to_plot], [coord[1] for coord in cur_data_to_plot], 10, colors[i], label = labels[i])
+
+            #Reset data to plot every loop.
+            cur_data_to_plot = []
+
+        #Plot the unidentified particles in distinct colors with their pdgs for labels.
+        for i in range(0, len(other_IDs)):
+            ID = other_IDs[i]
+            
+            #Extract x and y coordinates to plot only if they are all of the particle ID that we are currently plotting.
+            # [coord[0] for coord in other_to_plot if coord[2] == ID]
+            x = []
+            y = []
+            for coord in other_to_plot:
+                if coord[2] == ID:
+                    x.append(coord[0])
+                    y.append(coord[1])
+
+            plt.scatter(x, y, 10, other_colors[i], label = str(ID))
 
 
     # TODO This method needs to be adapted after first round of changes have been made.
